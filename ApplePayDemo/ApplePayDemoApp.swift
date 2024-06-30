d2d1fb27b8312f07bf010fcccd9e6b5068d0a122
Arda Nar <95191132+ardanar@users.noreply.github.com>
2024-06-30 11:04:53 +0300

    Initial Commit

---
+++ ApplePayDemo/ApplePayDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApplePayDemoApp.swift
+//  ApplePayDemo
+//
+//  Created by Arda Nar on 30.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct ApplePayDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
